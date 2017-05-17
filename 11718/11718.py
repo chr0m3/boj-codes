b1a4cd4a9e8ecf077279d3a36f971c378288f5cb
JunGu Kang <chr0m3@users.noreply.github.com>
2017-05-17 14:41:05 +0900

    Added 6 Codes
    Added 6 Codes
    - 1008
    - 10718
    - 10869
    - 10998
    - 11718
    - 11719

---
+++ 11718/11718.py
@@ -0,0 +1,6 @@
+while(True):
+    a = input()
+    if a:
+        print(a)
+    else:
+        break
---
+++ 11719/11719.py
@@ -0,0 +1,6 @@
+while(True):
+    a = input()
+    if a == 0:
+        break
+    else:
+        print(a)
