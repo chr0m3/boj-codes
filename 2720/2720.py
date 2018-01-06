7ea32be3037d1752a4e55109b1e2f1abd730225f
JunGu Kang <chr0m3@users.noreply.github.com>
2018-01-06 20:24:28 +0900

    Add 2720 Python

---
+++ 2720/2720.py
@@ -0,0 +1,12 @@
+test_cases = int(input())
+
+for i in range(0, test_cases):
+    money = int(input())
+    quarter = int(money / 25)
+    money %= 25
+    dime = int(money / 10)
+    money %= 10
+    nickel = int(money / 5)
+    penny = money % 5
+
+    print("%d %d %d %d" % (quarter, dime, nickel, penny))
