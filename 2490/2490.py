baad5302982bc0998d6557a8d81a0f2801543190
JunGu Kang <chr0m3@users.noreply.github.com>
2018-01-15 11:46:27 +0900

    Add 2490 Python

---
+++ 2490/2490.py
@@ -0,0 +1,12 @@
+for i in range(0, 3):
+    yuts = input().split().count('0')
+    if yuts == 0:
+        print('E')
+    elif yuts == 1:
+        print('A')
+    elif yuts == 2:
+        print('B')
+    elif yuts == 3:
+        print('C')
+    elif yuts == 4:
+        print('D')
