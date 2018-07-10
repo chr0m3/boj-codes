b5fa14bb50a0f371bceb7f60009d275df4cd8368
JunGu Kang <chr0m3@users.noreply.github.com>
2018-07-10 18:27:17 +0900

    Add 2941 Python

---
+++ 2941/2941.py
@@ -0,0 +1,2 @@
+print(len(str(input()).replace('c=', '0').replace('c-', '1').replace('dz=', '2').replace('d-', '3').replace('lj', '4')
+          .replace('nj', '5').replace('s=', '6').replace('z=', '7')))
