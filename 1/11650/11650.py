6d0535c29624074e5ee24344bc1939a1a9a4a1aa
JunGu Kang <chr0m3@users.noreply.github.com>
2020-02-27 21:44:29 +0900

    Add 11650 Python 3 Code

---
+++ 1/11650/11650.py
@@ -0,0 +1,20 @@
+from functools import cmp_to_key
+import sys
+
+
+def point_compare(a: tuple, b: tuple):
+    if a[0] == b[0]:
+        return a[1] - b[1]
+    return a[0] - b[0]
+
+
+input = sys.stdin.readline
+
+n = int(input())
+
+points = list()
+for i in range(n):
+    points.append(tuple(map(int, input().split())))
+
+for p in sorted(points, key=cmp_to_key(point_compare)):
+    print(f'{p[0]} {p[1]}')
