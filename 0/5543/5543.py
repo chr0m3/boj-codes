5a9623bae051738849d609731cb38805e6407061
JunGu Kang <chr0m3@users.noreply.github.com>
2020-03-01 21:33:54 +0900

    Add 5543 Python 3 Code

---
+++ 0/5543/5543.py
@@ -0,0 +1,8 @@
+burger1 = int(input())
+burger2 = int(input())
+burger3 = int(input())
+
+beverage1 = int(input())
+beverage2 = int(input())
+
+print(min(burger1, burger2, burger3) + min(beverage1, beverage2) - 50)
