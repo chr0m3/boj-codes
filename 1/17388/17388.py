3ba806f64abe4b07441c7fccacb25cf32448f6fb
JunGu Kang <chr0m3@users.noreply.github.com>
2020-03-06 22:51:08 +0900

    Add 17388 Python 3 Code

---
+++ 1/17388/17388.py
@@ -0,0 +1,10 @@
+s, k, h = map(int, input().split())
+
+if (s + k + h) >= 100:
+    print('OK')
+elif min(s, k, h) == s:
+    print('Soongsil')
+elif min(s, k, h) == k:
+    print('Korea')
+elif min(s, k, h) == h:
+    print('Hanyang')
